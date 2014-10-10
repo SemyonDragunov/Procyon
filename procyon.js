18d068df3586b5d42803961181e3f52896307134
EmperorSAM <sam31337@yandex.ru>
2014-10-10 22:49:11 +0600

    Add topbar mobile menu

--- procyon.js
+++ procyon.js
@@ -50,23 +50,22 @@
             $('[data-modal]').hide();
             $('.modal-bg').removeClass('open').fadeOut(500);
             $('body').removeClass('modal-open');
+	    return false;
         });
         
-        $('.modal-bg').bind('click', function() {
-            $('[data-modal]').hide();
+	$('.modal-bg').bind('click', function(event) {
+	    if ($(event.target).closest('[data-modal]').length) return;
+	    $('[data-modal]').hide();
             $('.modal-bg').removeClass('open').fadeOut(500);
             $('body').removeClass('modal-open');
-        });
-        
-        $('[data-modal]').bind('click', function() {
-            return false;
-        });
+	    event.stopPropagation();
+	});
         
         // Menu to anchor
         
         var lastId,
             topMenu = $('nav'),
-            topMenuHeight = topMenu.outerHeight() + 15,
+            topMenuHeight = topMenu.outerHeight(),
             menuItems = topMenu.find('a'),
             scrollItems = menuItems.map(function(){
                 var item = $($(this).attr('href'));
@@ -101,15 +100,53 @@
             }
         });
         
-        // Mobyle sidebar
+	// Topbar mobile
+	
+	var nameBack = '<<<<<<<';
+	var heightTopMenu = 50;
+	
+	$('.topbar .toggle').bind('click', function() {
+	    $(this).closest('.topbar').toggleClass('open').css('height', '').find('li.dropdown.open').removeClass('open');
+	});
+	
+	$('.topbar').find('li.dropdown > ul').prepend('<li class="back">' + nameBack + '</li>');
+	
+	$('.topbar li:not(.dropdown, .toggle, .back)').bind('click', function() {
+	    $(this).closest('.topbar').css('height', '').removeClass('open').find('li.dropdown.open').removeClass('open');
+	});
+	
+	$.fn.toggleMenu = function() {
+	    var heightMenu = this.parent('li.dropdown').children('ul').outerHeight(true) + heightTopMenu;
+	    
+	    this.parent('li.dropdown').toggleClass('open')
+	    this.closest('.topbar').css('height', heightMenu);
+	}
+	
+	$('.topbar li.dropdown > a').bind('click', function() {
+	    $(this).toggleMenu();
+	});
+	
+	$('.back').bind('click', function() {
+	    var heightMenu = $(this).parent('ul').parent('li.dropdown').parent('ul').outerHeight(true) + heightTopMenu;
+	    
+	    $(this).parent('ul').parent('li.dropdown').removeClass('open').find('.open').removeClass('open');
+	    if ($(this).closest('li.dropdown').parent('ul').parent('li.dropdown').length) {
+		$(this).closest('.topbar').css('height', heightMenu);
+	    } else {
+		$(this).closest('.topbar').css('height', '');
+	    }
+	});
+	
+        // Mobyle bar
         
-        $('.sidebar-menu').bind('click', function() {
-            if ($('.sidebar-open').length) {
-                $('[data-wrapper]').removeClass('sidebar-open');
+        $('[data-topbar] > .toggle').bind('click', function() {
+            if ($('.mobilebar-open').length) {
+                $('[data-wrapper]').removeClass('mobilebar-open');
             } else {
-                $('[data-wrapper]').addClass('sidebar-open');
+                $('[data-wrapper]').addClass('mobilebar-open');
             }
         });
+	
     });
     
 })(jQuery);
