f810a46917f7ef35c0401eb69fd9d770a4b3e21d
EmperorSAM <sam31337@yandex.ru>
2014-10-12 00:40:57 +0600

    Fix topbar mobile menu levels

--- procyon.js
+++ procyon.js
@@ -6,8 +6,13 @@
 
 (function ($) {
     
+    function widDoc() {
+	var winWidth = $(window).width();
+	return winWidth;
+    }
+    
     $(document).ready(function () {
-        
+	
         // Spoiler
         
         $('.spoiler-head').bind('click', function() {
@@ -21,7 +26,7 @@
         $('span.close').bind('click', function() {
             $(this).parent().fadeOut(500);
         });
-        
+	
         // Modal window in HTML
         
         $('[data-modal]').wrap('<div class="modal-bg"></div>');
@@ -60,76 +65,60 @@
             $('body').removeClass('modal-open');
 	    event.stopPropagation();
 	});
-        
-        // Menu to anchor
-        
-        var lastId,
-            topMenu = $('nav'),
-            topMenuHeight = topMenu.outerHeight(),
-            menuItems = topMenu.find('a'),
-            scrollItems = menuItems.map(function(){
-                var item = $($(this).attr('href'));
-                if (item.length) { return item; }
-            });
-
-
-        menuItems.click(function(e){
-            var href = $(this).attr('href'),
-                offsetTop = href === '#' ? 0 : $(href).offset().top-topMenuHeight + 1;
-            $('html, body').stop().animate({
-                scrollTop: offsetTop
-            }, 1000, 'swing');
-            e.preventDefault();
-        });
-
-        $(window).scroll(function(){
-            var fromTop = $(this).scrollTop()+topMenuHeight;
-
-            var cur = scrollItems.map(function(){
-                if ($(this).offset().top < fromTop)
-                    return this;
-            });
-            cur = cur[cur.length-1];
-            var id = cur && cur.length ? cur[0].id : "";
-
-            if (lastId !== id) {
-                lastId = id;
-                menuItems
-                    .removeClass('active').parent().removeClass('active')
-                    .end().filter('[href=#'+id+']').addClass('active').parent().addClass('active');
-            }
-        });
-        
-	// Topbar mobile
 	
-	var nameBack = '<<<<<<<';
-	var heightTopMenu = 50;
+	// Topbar mobile
+
+	var nameBack = '<<<<<<<',
+	    heightTopMenu = 50;
+	
+	$('.topbar li.dropdown > ul').prepend('<li class="back">' + nameBack + '</li>');
+	
+	$('.topbar li.dropdown > a').addClass('no-click');
 	
 	$('.topbar .toggle').bind('click', function() {
-	    $(this).closest('.topbar').toggleClass('open').css('height', '').find('li.dropdown.open').removeClass('open');
+	    var $topbar = $(this).closest('.topbar');
+	    
+	    $topbar.toggleClass('open').css('height', '').find('li.dropdown.open').removeClass('open');
+	    
+	    if ($topbar.is('.fixed-top')) {
+		var fixedMenu = true;
+	    }
+	    
+	    if ($topbar.outerHeight() > $(window).height() && fixedMenu) {
+		$topbar.css({'bottom' : '0' , 'overflow-y' : 'scroll'});
+	    } else {
+		$topbar.css({'bottom' : '' , 'overflow-y' : 'hidden'});
+	    }
 	});
 	
-	$('.topbar').find('li.dropdown > ul').prepend('<li class="back">' + nameBack + '</li>');
+	$('.topbar input:not([type="submit"])').bind('click', function() {
+	    return false;
+	});
 	
 	$('.topbar li:not(.dropdown, .toggle, .back)').bind('click', function() {
 	    $(this).closest('.topbar').css('height', '').removeClass('open').find('li.dropdown.open').removeClass('open');
 	});
 	
-	$.fn.toggleMenu = function() {
-	    var heightMenu = this.parent('li.dropdown').children('ul').outerHeight(true) + heightTopMenu;
+	$('.topbar li.dropdown > a').bind('click', function(e) {	    
+	    if (widDoc() <= 767) {
+		var heightMenu = $(this).parent('li.dropdown').children('ul').outerHeight(true) + heightTopMenu;
+		$(this).parent('li.dropdown').toggleClass('open');
+		$(this).closest('.topbar').css('height', heightMenu);
+	    }
 	    
-	    this.parent('li.dropdown').toggleClass('open')
-	    this.closest('.topbar').css('height', heightMenu);
-	}
-	
-	$('.topbar li.dropdown > a').bind('click', function() {
-	    $(this).toggleMenu();
+	    if (window.location.hash) {
+		e.preventDefault();
+	    }
+	    
+	    e.preventDefault();
 	});
 	
 	$('.back').bind('click', function() {
-	    var heightMenu = $(this).parent('ul').parent('li.dropdown').parent('ul').outerHeight(true) + heightTopMenu;
+	    if (widDoc() <= 767) {
+		var heightMenu = $(this).parent('ul').parent('li.dropdown').parent('ul').outerHeight(true) + heightTopMenu;
+		$(this).parent('ul').parent('li.dropdown').removeClass('open').find('.open').removeClass('open');
+	    }
 	    
-	    $(this).parent('ul').parent('li.dropdown').removeClass('open').find('.open').removeClass('open');
 	    if ($(this).closest('li.dropdown').parent('ul').parent('li.dropdown').length) {
 		$(this).closest('.topbar').css('height', heightMenu);
 	    } else {
@@ -144,6 +133,45 @@
                 $('[data-wrapper]').removeClass('mobilebar-open');
             } else {
                 $('[data-wrapper]').addClass('mobilebar-open');
+            }
+        });
+	
+	// Menu to anchor
+        
+        var lastId,
+            topMenu = $('nav'),
+            topMenuHeight = topMenu.outerHeight() + 15,
+            menuItems = topMenu.find('a:not(.no-click)'),
+            scrollItems = menuItems.map(function(){
+                var item = $($(this).attr('href'));
+                if (item.length) { return item; }
+            });
+
+
+        menuItems.click(function(e) {
+            var href = $(this).attr('href'),
+                offsetTop = href === '#' ? 0 : $(href).offset().top-topMenuHeight + 1;
+            $('html, body').stop().animate({
+                scrollTop: offsetTop
+            }, 1000, 'swing');
+            e.preventDefault();
+        });
+
+        $(window).scroll(function() {
+            var fromTop = $(this).scrollTop()+topMenuHeight;
+
+            var cur = scrollItems.map(function(){
+                if ($(this).offset().top < fromTop)
+                    return this;
+            });
+            cur = cur[cur.length-1];
+            var id = cur && cur.length ? cur[0].id : "";
+
+            if (lastId !== id) {
+                lastId = id;
+                menuItems
+                    .removeClass('active').parent().removeClass('active')
+                    .end().filter('[href=#'+id+']').addClass('active').parent().addClass('active');
             }
         });
 	
